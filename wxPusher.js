db98413b2aef2ca3305e1d637b911458e8b4943d
forkWo <119728345+forkWo@users.noreply.github.com>
2024-01-31 07:27:40 +0800

    Update wxPusher.js

--- wxPusher.js
+++ wxPusher.js
@@ -1,4 +1,4 @@
 module.exports = {
     appToken: process.env.WX_PUSHER_APP_TOKEN || 'AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr',
-    uid: process.env.WX_PUSHER_UID||''
+    uid: process.env.WX_PUSHER_UID||'UID_c2jl3s56pnSFSijTKFPxgv8iJAL5'
   };
